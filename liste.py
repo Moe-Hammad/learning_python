85b979f14642e35defb2f3ad8594a95d01ed1158
Bemast3r <dahempvp@gmail.com>
2025-06-25 17:01:16 +0200

    Liste

---
+++ liste.py
@@ -0,0 +1,14 @@
+############# Liste
+leere_liste = ["Apfel", "Birne", "Orange", "Zitrone"]
+print(leere_liste)
+leere_liste.append("Trauben")
+print(leere_liste)
+leere_liste.insert(0, "Pflaume")
+print(leere_liste)
+leere_liste.remove("Apfel")
+print(leere_liste)
+leere_liste.pop()
+print(leere_liste)
+del leere_liste[0]
+print(leere_liste)
+#################################
