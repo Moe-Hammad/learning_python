e268d6734034386fe1d1f57b4609b4e9e1324d0f
Bemast3r <dahempvp@gmail.com>
2025-06-25 16:44:58 +0200

    Bool

---
+++ bool.py
@@ -0,0 +1,12 @@
+print(2 < 1)
+print("s12s")
+print(1 == 1)
+
+streckeinKm = int(input("Gebe die Strecke in Km ein: "))
+
+if streckeinKm > 15:
+    print("Fahr Auto")
+elif streckeinKm == 10:
+    print("Sowohl Fahrrad als Auto.")
+else:
+    print("Fahr Fahrrad")
