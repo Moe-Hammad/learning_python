47bebabf67e124d1d5278efaa3195fbe4157a8c9
Bemast3r <dahempvp@gmail.com>
2025-06-25 15:28:20 +0200

    integer, input und bool

--- int.py
+++ int.py
@@ -4,5 +4,12 @@
 print(1-0)
 #Hoch
 print(10 ** 2 )
- 
+###########################################
+
+xi = input("Wie ist dein Name?")
+print(xi)
+y = input("Wie viel ist dein Ausgangsgehalt? ")
+ya = float(y) * 1.10
+print(ya)
+
 
---
+++ umrechner.py
@@ -0,0 +1,3 @@
+intro = float(input("Gebe die Summe welche du von Euro in USD umwandeln willst ein. "))
+usd = intro * 1.15
+print(str(intro) , "in USD sind " , str(usd))
