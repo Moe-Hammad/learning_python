12d41f21187e6c12998e1b62ece476878769c7db
Bemast3r <dahempvp@gmail.com>
2025-06-25 17:08:06 +0200

    Tupel

---
+++ tupel.py
@@ -0,0 +1,7 @@
+andereliste = ["Apfel", "Birne", "Orange", "Zitrone"]  ###liste
+andereliste[0] = "Hund"
+print(andereliste)
+
+######## Tupel
+tup = ("Apfel", "Birne", "Orange", "Zitrone")
+# tup[1] = "Hund" geht nicht!
